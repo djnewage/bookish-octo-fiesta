9988d7e067cf0cb1ca4cf51d2eb90e1ca1dbbb63
djnewage <djnewage1220@gmail.com>
2018-11-04 20:50:59 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+function users(user1, user2){
+    const user1 = [{
+        name: 'john',
+        age: 25,
+        car: 'mazda',
+    }];
+
+    const user2 = [{
+        name: 'Kev',
+        age: 30,
+        car: 'ford',
+    }];
+    
+},
