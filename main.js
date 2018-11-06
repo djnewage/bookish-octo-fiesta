08322da5d7de7450c3351c79cc33eee1e7c4977f
djnewage <djnewage1220@gmail.com>
2018-11-05 21:27:59 -0600

    Update main.js

--- main.js
+++ main.js
@@ -12,3 +12,49 @@ function users(user1, user2){
     }];
     
 },
+
+    
+    // let database = [
+//     {
+//         username: 'Tristan',
+//         password: 'Dragon',
+//     }
+// ];
+
+// let newsFeed = [
+//     {
+//         username: "bobby",
+//         timeline: 'So tired from all that javascript',
+//     },
+//     {
+//         username: 'Sally',
+//         timeline: 'Javascript is so cool'
+//     }
+// ];
+
+
+// let userNamePrompt = prompt('Whats your username?');
+// let passwordPrompt = prompt('Whats your password?');
+
+// function signIn(x, y){
+//     if(x === database[0].username && y === database[0].password){
+//         console.log(newsFeed);
+//     }else {
+//         alert('Sorry wrong username and password!');
+//     }
+// };
+
+
+// signIn(userNamePrompt, passwordPrompt);
+
+let todos = [
+    'Clean room',
+    'Do Laundry',
+    'Brush teeth',
+    'Paint Walls',
+    'Play video games'
+];
+
+for (var i = 0; i < todos.length; i++){
+    console.log(todos.length);
+}
